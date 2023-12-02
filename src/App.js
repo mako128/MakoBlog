3a2b61058316022cf32647f6e42c435e4ea52974
Makovitch Tchaptchey-Konda <102027189+mako128@users.noreply.github.com>
2023-12-02 10:28:41 +0000

    MakoBlog First Commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import Blogs from "./Components/Blogs";
+import Homepage from "./Components/Homepage";
+import Navbar from "./Components/Navbar";
+
+import { selectSignedIn } from "./features/userSlice";
+import "./styling/app.css";
+
+
+function App(){
+  const isSignedIn = useSelector(selectSignedIn);
+
+  return (
+    <div className="app">
+      <Navbar />
+      <Homepage />
+      {isSignedIn && <Blogs />}
+      
+    
+
+</div>
+);
+}
+
+export default App;
---
+++ src/Components/Navbar.js
@@ -0,0 +1,117 @@
+import { Avatar, Modal } from "@material-ui/core";
+import React, { useState } from "react";
+import { GoogleLogout } from "react-google-login";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  selectSignedIn,
+  selectUserData,
+  setInput,
+  setSignedIn,
+  setUserData,
+} from "../features/userSlice";
+
+import "../styling/navbar.css";
+
+const Navbar = () => {
+  const [inputValue, setInputValue] = useState("tech");
+  const isSignedIn = useSelector(selectSignedIn);
+  const userData = useSelector(selectUserData);
+
+  const dispatch = useDispatch();
+
+  const logout = (response) => {
+    dispatch(setSignedIn(false));
+    dispatch(setUserData(null));
+  };
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    dispatch(setInput(inputValue));
+  };
+
+  function MyVerticallyCenteredModal(props) {
+    return (
+      <Modal
+        {...props}
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="contained-modal-title-vcenter">
+            Modal heading
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <h4>Centered Modal</h4>
+          <p>
+            Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
+            dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
+            consectetur ac, vestibulum at eros.
+          </p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button onClick={props.onHide}>Close</Button>
+        </Modal.Footer>
+      </Modal>
+    );
+  }
+
+  return (
+      
+    <div className="navbar">
+      <h1 className="navbar__header">MakoBlog 💬</h1>
+      {isSignedIn && (
+        <div className="blog__search">
+          <input
+            className="search"
+            placeholder="Search for a blog"
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
+          />
+          <button className="submit" onClick={handleClick}>
+            Search
+          </button>
+        </div>
+      )}
+
+      {isSignedIn ? (
+        <div className="navbar__user__data">
+
+
+<Button variant="primary" onClick={MyVerticallyCenteredModal}>
+          <Avatar 
+            className="user"
+            
+            src={userData?.imageUrl}
+            alt={userData?.name}
+            
+          />
+          </Button>
+      
+
+          
+
+          <h1 className="signedIn">{userData?.givenName}</h1>
+          <GoogleLogout
+            clientId="775199807183-lhdvl8b52koa7uvjujgjdf7t02cjlbd4.apps.googleusercontent.com"
+            render={(renderProps) => (
+              <button
+                onClick={renderProps.onClick}
+                disabled={renderProps.disabled}
+                className="logout__button"
+              >
+                Logout 😦
+              </button>
+            )}
+            onLogoutSuccess={logout}
+          />
+        </div>
+      ) : (
+        <h1 className="notSignedIn">User not available 😞</h1>
+      )}
+    </div>
+  );
+};
+
+export default Navbar;
